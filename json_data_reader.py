3e708e20a07610e763ae76e4f31df9dcf5bdf5ae
larribas24 <50624637+larribas24@users.noreply.github.com>
2019-11-04 06:31:18 +0100

    Add files via upload

--- json_data_reader.py
+++ json_data_reader.py
@@ -22,18 +22,18 @@ def run():
     print('reading buffer...')
     with open(buffer, encoding='utf-8-sig') as json_file:
         json_data = json.load(json_file)
+    #remove ¨opening and closing { } 
     json_data = json_data[1 : len(json_data)]
-    lines = json_data.split("\n")
-        
+    #split string in lines separated by carriage return
+    lines = json_data.split("\n")        
     #remove comas
-    lines = list(map(lambda s: s[:-1], lines))
-       
+    lines = list(map(lambda s: s[:-1], lines))       
     #get columns names and build empty dataframe
     lineDict = json.loads(lines[0])
+    #create empty dataframe
     df = pd.DataFrame(columns = lineDict.keys() ) 
     
     for line in lines:
-        #print(line)
         lineDict = json.loads(line)
         df = df.append(lineDict , ignore_index=True)
     df.set_index(keys = 'key', inplace = True) 
